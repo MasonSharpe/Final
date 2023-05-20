dfe6b3d68dd1096fc94910bc7c6d62f917327225
Mason <115174711+MasonSharpe@users.noreply.github.com>
2023-05-19 17:02:34 -0700

    ending

--- Final!/Assets/Scripts/LevelEnd.cs
+++ Final!/Assets/Scripts/LevelEnd.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
     GameManager gameManager;
+    float shrinkTimer = 0;
     void Start()
     {
         gameManager = GetComponentInParent<GameManager>();
@@ -13,10 +15,35 @@ public class LevelEnd : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Vector2 dist = (gameManager.player.transform.position - transform.position);
+        shrinkTimer -= Time.deltaTime / 3;
+        if (shrinkTimer < 1 && shrinkTimer > 0.1f)
+        {
+            float mult = (shrinkTimer) * 0.438f;
+            gameManager.player.transform.localScale = new Vector3(mult, mult, 1);
+            if (shrinkTimer < 0.2f)
+            {
+                SceneManager.LoadScene("dsad");
+                shrinkTimer = 0;
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        Vector2 dist = (gameManager.player.transform.position - transform.position);
         if (dist.magnitude < 10)
         {
-            gameManager.playerInfo.rb.velocity += -dist;
+            gameManager.playerInfo.rb.velocity += -dist / 2;
+
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (shrinkTimer < 0)
+        {
+            shrinkTimer = 1;
         }
     }
 }
